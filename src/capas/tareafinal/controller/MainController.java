f56e5a26408d0ea1d85418c26252926fa312aedb
kevin <00251716@uca.edu.sv>
2019-06-22 19:15:38 -0600

    database backup file added

--- src/capas/tareafinal/controller/MainController.java
+++ src/capas/tareafinal/controller/MainController.java
@@ -1,12 +1,7 @@
 package capas.tareafinal.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import capas.tareafinal.service.EmployeeService;
-import capas.tareafinal.service.SucursalService;
-import capas.tareafinal.service.UserService;
 
 @Controller
 public class MainController {
@@ -16,5 +11,7 @@ public class MainController {
 	public String initMain(){
 		return "login";
 	}
+	
+	
 	
 }
