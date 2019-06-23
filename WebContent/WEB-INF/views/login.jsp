4d9986cae222b6a8c5de1db7c1dc96374049bfd8
kevin <00251716@uca.edu.sv>
2019-06-22 19:11:40 -0600

    login.jsp added

---
+++ WebContent/WEB-INF/views/login.jsp
@@ -0,0 +1,26 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<link rel="stylesheet" href="resources/style.css">
+	<meta charset="UTF-8">
+	<title>Bienvenido</title>
+</head>
+
+<body>
+
+<div class="login-page">
+  <div class="form">
+    <form class="login-form">
+      <input type="text" placeholder="Nombre de usuario"/>
+      <input type="password" placeholder="Contraseña"/>
+      <button>Iniciar sesión</button>
+    </form>
+  </div>
+</div>
+
+</body>
+</html>
--- src/capas/tareafinal/controller/MainController.java
+++ src/capas/tareafinal/controller/MainController.java
@@ -2,8 +2,7 @@ package capas.tareafinal.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import capas.tareafinal.service.EmployeeService;
 import capas.tareafinal.service.SucursalService;
@@ -12,20 +11,10 @@ import capas.tareafinal.service.UserService;
 @Controller
 public class MainController {
 
-	@Autowired
-	EmployeeService eService;
-	
-	@Autowired
-	SucursalService sService;
-	
-	@Autowired
-	UserService uService;
-	
-	@RequestMapping("/")
-	public ModelAndView initMain(){
-		ModelAndView mav = new ModelAndView();
-		
-		return mav;
+
+	@GetMapping("/")	
+	public String initMain(){
+		return "login";
 	}
 	
 }
